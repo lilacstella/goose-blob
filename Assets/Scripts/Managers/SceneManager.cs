524ffa171d1685a413c28c0845071f48eff23bc0
EntropicEquanimity <joshuahunt25@hotmail.com>
2025-09-04 23:54:12 -0600

    Added Scene Manager / Scene Switching

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -1,17 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
     public static SceneManager Instance;
-    
-    void Start()
+
+    public Animator sceneChangeAnimator;
+
+    private void Awake()
     {
-        
+        if (Instance == null) { Instance = this; DontDestroyOnLoad(this); }
+        else { Destroy(gameObject); }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha0)) { SwitchScene(1); }
+        if(Input.GetKeyDown(KeyCode.Alpha1)) { SwitchScene(2); }
+    }
+
+    public void SwitchScene(int scene)
+    {
+        StartCoroutine(WaitScene(sceneChangeAnimator, scene));
+
+        IEnumerator WaitScene(Animator animator, int scene)
+        {
+            animator.Play("Start");
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+            animator.Play("End");
+        }
     }
 }
